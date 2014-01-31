a4a196c9376c4dda63d652a9c333c707d0e1a381
Matthew Kiernan <matthewkiernan@Matthew-Kiernans-MacBook-Pro.local>
2014-01-31 10:42:09 -0500

    Initial Commit

---
+++ UDM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UDM
+//
+//  Created by Matthew Kiernan on 1/31/14.
+//  Copyright (c) 2014 Matthew Kiernan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
